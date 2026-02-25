2a886002d0ce2c3f4ef6470bd92b44f64ffe0247
Joey Cupp <youremail@example.com>
2026-02-25 14:40:59 +0100

    Fix formatting and update author information in main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: jcupp <jcupp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 10:00:00 by joeyscags         #+#    #+#             */
-/*   Updated: 2026/02/24 20:04:35 by joeyscags        ###   ########.fr       */
+/*   Updated: 2026/02/25 14:40:20 by jcupp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,18 @@ int main(void)
 	std::cout << "=== Base class type identification test ===" << std::endl;
 	std::cout << std::endl;
 
-	// Test 1: Generate and identify with pointer
 	std::cout << "--- Test 1: Pointer identification ---" << std::endl;
 	Base* obj1 = generate();
 	std::cout << "Identified via pointer: ";
 	identify(obj1);
 	std::cout << std::endl;
 
-	// Test 2: Identify with reference
 	std::cout << "--- Test 2: Reference identification ---" << std::endl;
 	std::cout << "Identified via reference: ";
 	identify(*obj1);
 	std::cout << std::endl;
 	delete obj1;
 
-	// Test 3: Multiple random objects
 	std::cout << "--- Test 3: Multiple random objects ---" << std::endl;
 	for (int i = 0; i < 5; i++)
 	{
